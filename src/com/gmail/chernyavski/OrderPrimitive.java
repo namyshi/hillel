3cf5791643e95077ef6540c4b9c7dfe3fc2a9af4
Alexander <nywkin2016@gmail.com>
2025-04-22 19:28:52 +0200

    initial commit

---
+++ src/com/gmail/chernyavski/OrderPrimitive.java
@@ -0,0 +1,28 @@
+package com.gmail.chernyavski;
+
+public class OrderPrimitive {
+    private static final String EUR = "EUR";
+
+    public static void main(String[] args) {
+        String clientName = "Alice";
+        String product = "smartphone";
+        double price = 305.99;
+        String address = "Moon Street, 10";
+
+        System.out.println("Order No 1 Client: " + clientName + ".");
+        System.out.println("Product: " + product + ",");
+        System.out.println("price " + EUR + " " + price + ".");
+        System.out.println("Address: " + address + ".");
+
+        clientName = "Tom";
+        product = "laptop";
+        price = 570.95;
+        address = "Terra Street, 17";
+
+        System.out.println("Order No 2 Client: " + clientName + ".");
+        System.out.println("Product: " + product + ",");
+        System.out.println("price " + EUR + " " + price + ".");
+        System.out.println("Address: " + address + ".");
+
+    }
+}
